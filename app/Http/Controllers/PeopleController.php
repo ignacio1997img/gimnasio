ce7a23f1df54a0895ed4bef8978a385c53eaf0ce
ignacio1997img <ignaciomolinaguzman20@gmail.com>
2023-03-02 22:33:25 -0400

    si

--- app/Http/Controllers/PeopleController.php
+++ app/Http/Controllers/PeopleController.php
@@ -11,7 +11,7 @@ class PeopleController extends Controller
 {
     public function index()
     {
-        $data = DB::connection('mysqls')->table('people')->get();
+        $data = DB::connection('mysqls')->table('contratos')->get();
         dump($data);
         $data = DB::table('dbo.Personas')->get();
         dump($data);
